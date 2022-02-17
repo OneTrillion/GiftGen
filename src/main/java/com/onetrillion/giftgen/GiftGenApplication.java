47a0209fbc263d0601b4e3009437e5a07a0fda7b
One <tradgardsmastaren@protonmail.com>
2022-02-17 16:58:13 +0100

    created project

---
+++ src/main/java/com/onetrillion/giftgen/GiftGenApplication.java
@@ -0,0 +1,13 @@
+package com.onetrillion.giftgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onetrillion/giftgen/GiftGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetrillion.giftgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
