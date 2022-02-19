47120fd663e5206ededd2173a455421842fa249e
onetrillion <tradgardsmastaren@protonmail.com>
2022-02-20 00:12:58 +0100

    Added form and submit to frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import {Row, Col, Button, Form} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState("black");
+
+    const handleOnChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const asd = () => {
+        console.log(value);
+    }
+
   return (
     <div className="App">
+        <Row>
+            <Col><h1>Placeholder</h1></Col>
+        </Row>
+       <Row>
+           <Col><Form.Control as="select" custom onChange={handleOnChange} defaultValue={value}>
+               <option value="random">Random</option>
+               <option value="parent">Parent</option>
+               <option value="partner">Partner</option>
+               <option value="friend">Friend</option>
+           </Form.Control></Col>
+            <Col>
+                <Button variant="success" onClick={asd}>Success</Button>
+            </Col>
+       </Row>
 
     </div>
   );
